10773e9868d3d68a2a5bc807041a41396ce25d3f
Beat Walti <beat.walti@adesso.ch>
2019-01-11 16:28:34 +0100

    started to connect to all services through Gateway
    - have to define settings for base URL etc for each service

---
+++ src/Gateway/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Gateway
+{
+    using System.IO;
+
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                          .UseContentRoot(Directory.GetCurrentDirectory())
+                          .ConfigureAppConfiguration((hostingContext, config) =>
+                          {
+                              config
+                                  .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                                  .AddJsonFile("appsettings.json", true, true)
+                                  .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                                  .AddJsonFile("ocelot.json")
+                                  .AddEnvironmentVariables();
+                          })
+                          .UseStartup<Startup>();
+        }
+    }
+}
---
+++ src/Gateway/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace Gateway
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
+    using Ocelot.DependencyInjection;
+    using Ocelot.Middleware;
+
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(config =>
+            {
+                config.AddConsole();
+            });
+
+            services.AddOcelot();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseOcelot()
+               .Wait();
+        }
+    }
+}
